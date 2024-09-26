8efe8b9224908e38843fea7c6392d3f318033e63
Simranpal Kohli <simranpalkh20@gmail.com>
2024-09-26 19:16:15 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,80 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+import base64
+import streamlit as st
+import os
+import io
+from PIL import Image 
+import pdf2image
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(input,pdf_cotent,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,pdf_content[0],prompt])
+    return response.text
+
+def input_pdf_setup(uploaded_file):
+    if uploaded_file is not None:
+        ## Convert the PDF to image
+        images=pdf2image.convert_from_bytes(uploaded_file.read())
+
+        first_page=images[0]
+
+        # Convert to bytes
+        img_byte_arr = io.BytesIO()
+        first_page.save(img_byte_arr, format='JPEG')
+        img_byte_arr = img_byte_arr.getvalue()
+
+        pdf_parts = [
+            {
+                "mime_type": "image/jpeg",
+                "data": base64.b64encode(img_byte_arr).decode()  # encode to base64
+            }
+        ]
+        return pdf_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+## Streamlit App
+
+st.set_page_config(page_title="Code Snippet")
+st.header("Interview Questions Generation")
+input_text=st.text_area("Code Query: ",key="input")
+uploaded_file=st.file_uploader("Upload reference file as PDF...",type=["pdf"])
+
+
+if uploaded_file is not None:
+    st.write("PDF Uploaded Successfully")
+
+
+submit1 = st.button("Generate Questions snippets")
+
+
+
+input_prompt1 = """
+ You're a Sky Company interviewer who has assigned me some analysis technical based task.
+ can you help me generate python code scripts for the desired pdf questions.
+ Please remember based on this python scripts analysis, my interview is based.
+"""
+
+
+
+if submit1:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt1,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please uplaod the PDF")
+
+
+   
+
+
+
+
+
